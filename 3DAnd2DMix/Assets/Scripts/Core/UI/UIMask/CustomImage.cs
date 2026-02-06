e2d99a2a0fbc433ce8ddb5d5a5d3ffef653973c8
TonyTang <435853363@qq.com>
2026-02-06 17:23:18 +0800

    Feat: 1. 支持穿透Image

---
+++ 3DAnd2DMix/Assets/Scripts/Core/UI/UIMask/CustomImage.cs
@@ -0,0 +1,93 @@
+/*
+ * Description:             CustomImage.cs
+ * Author:                  TONYTANG
+ * Create Date:             2026/02/05
+ */
+
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+/// <summary>
+/// CustomImage.cs
+/// 自定义Image用于支持反向遮罩和透明点击穿透设置
+/// </summary>
+public class CustomImage : Image
+{
+    /// <summary>
+    /// 是否开启反向遮罩
+    /// </summary>
+    [Header("是否开启反向遮罩")]
+    public bool EnableInvertMask = false;
+
+    /// <summary>
+    /// 是否激活透明Alpha透明可点击阈值
+    /// </summary>
+    [Header("是否激活透明Alpha透明可点击阈值")]
+    public bool EnableAlphaHitTestMinimusThreshold = false;
+
+    /// <summary>
+    /// 透明Alpha可点击阈值(<=0表示全部可点击，>1表示全不可点击，其他值表示小于该值不可点击)
+    /// Note：
+    /// 仅当EnableAlphaHitTestMinimusThreshold=true时有效
+    /// </summary>
+    [Header("透明Alpha可点击阈值")]
+    public float AlphaHitTestMinimumThreshold = 0.1f;
+
+    protected override void Start()
+    {
+        base.Start();
+        UpdateAlphaHitTestMinimumThreshold();
+    }
+
+    /// <summary>
+    /// 更新透明Alpha可点击阈值
+    /// Note:
+    /// 外部修改EnableAlphaHitTestMinimusThreshold或AlphaHitTestMinimumThreshold值后
+    /// 请调用此方法确保更新成功
+    /// </summary>
+    public void UpdateAlphaHitTestMinimumThreshold()
+    {
+        alphaHitTestMinimumThreshold = EnableAlphaHitTestMinimusThreshold ? AlphaHitTestMinimumThreshold : 0f;
+    }
+    
+    /// <summary>
+    /// See IMaterialModifier.GetModifiedMaterial
+    /// </summary>
+    public override Material GetModifiedMaterial(Material baseMaterial)
+    {
+        if(!EnableInvertMask)
+        {
+            return base.GetModifiedMaterial(baseMaterial);
+        }
+
+        return GetInvertMaskModifiedMaterial(baseMaterial);
+    }
+
+    /// <summary>
+    /// 获取反向遮罩修改材质球
+    /// </summary>
+    /// <param name="baseMaterial"></param>
+    /// <returns></returns>
+    protected Material GetInvertMaskModifiedMaterial(Material baseMaterial)
+    {
+        var toUse = baseMaterial;
+
+        if (m_ShouldRecalculateStencil)
+        {
+            var rootCanvas = MaskUtilities.FindRootSortOverrideCanvas(transform);
+            m_StencilValue = maskable ? MaskUtilities.GetStencilDepth(transform, rootCanvas) : 0;
+            m_ShouldRecalculateStencil = false;
+        }
+
+        Mask maskComponent = GetComponent<Mask>();
+        if (m_StencilValue > 0 && (maskComponent == null || !maskComponent.IsActive()))
+        {
+            var maskMat = StencilMaterial.Add(toUse, (1 << m_StencilValue) - 1, StencilOp.Keep, CompareFunction.NotEqual, ColorWriteMask.All, (1 << m_StencilValue) - 1, 0);
+            StencilMaterial.Remove(m_MaskMaterial);
+            m_MaskMaterial = maskMat;
+            toUse = m_MaskMaterial;
+        }
+        return toUse;
+    }
+}
--- 3DAnd2DMix/Assets/Scripts/Editor/Core/UI/UIMask/CustomImageEditor.cs
+++ 3DAnd2DMix/Assets/Scripts/Editor/Core/UI/UIMask/CustomImageEditor.cs
@@ -1,5 +1,5 @@
 /*
- * Description:             PenetrateImageEditor.cs
+ * Description:             CustomImageEditor.cs
  * Author:                  TONYTANG
  * Create Date:             2026/02/06
  */
@@ -11,26 +11,32 @@ using UnityEditor.UI;
 using UnityEngine;
 
 /// <summary>
-/// PenetrateImageEditor.cs
-/// PenetrateImage的Inspector扩展
+/// CustomImageEditor.cs
+/// CustomImage的Inspector扩展
 /// </summary>
-[CustomEditor(typeof(PenetrateImage))]
+[CustomEditor(typeof(CustomImage))]
 [CanEditMultipleObjects]
-public class PenetrateImageEditor : ImageEditor
+public class CustomImageEditor : ImageEditor
 {
     /// <summary>
-    /// 是否激活透明Alpha穿透阈值属性
+    /// 是否开启反向遮罩属性
+    /// </summary>
+    private SerializedProperty mEnableInvertMask;
+
+    /// <summary>
+    /// 是否激活透明Alpha透明可点击阈值属性
     /// </summary> <summary>
     private SerializedProperty mEnableAlphaHitTestMinimusThreshold;
 
     /// <summary>
-    /// 透明Alpha穿透阈值属性
+    /// 透明Alpha可点击阈值属性
     /// </summary>
     private SerializedProperty mAlphaHitTestMinimumThreshold;
     
     protected override void OnEnable()
     {
         base.OnEnable();
+        mEnableInvertMask = serializedObject.FindProperty("EnableInvertMask");
         mEnableAlphaHitTestMinimusThreshold = serializedObject.FindProperty("EnableAlphaHitTestMinimusThreshold");
         mAlphaHitTestMinimumThreshold = serializedObject.FindProperty("AlphaHitTestMinimumThreshold");
     }
@@ -41,6 +47,8 @@ public class PenetrateImageEditor : ImageEditor
 
         serializedObject.Update();
 
+        EditorGUILayout.PropertyField(mEnableInvertMask);
+
         EditorGUI.BeginChangeCheck();
         EditorGUILayout.PropertyField(mEnableAlphaHitTestMinimusThreshold);
         bool enableChanged = EditorGUI.EndChangeCheck();
@@ -49,7 +57,6 @@ public class PenetrateImageEditor : ImageEditor
         EditorGUILayout.PropertyField(mAlphaHitTestMinimumThreshold);
         bool thresholdChanged = EditorGUI.EndChangeCheck();
 
-        // 应用序列化属性更改
         serializedObject.ApplyModifiedProperties();
 
         if (enableChanged || thresholdChanged)
@@ -69,7 +76,7 @@ public class PenetrateImageEditor : ImageEditor
         }
         foreach (var targetObj in targets)
         {
-            var penetrateImage = targetObj as PenetrateImage;
+            var penetrateImage = targetObj as CustomImage;
             if (penetrateImage != null)
             {
                 penetrateImage.UpdateAlphaHitTestMinimumThreshold();
